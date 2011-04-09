f0d57676bfd4288d1b35ea32c7939c865d9185ba
Chetan T <chetant@gmail.com>
2011-04-09 09:20:57 -0400

    Minor cleanup, making inclusionVisitor more accessible

--- test/Test_Diagnostics.hs
+++ test/Test_Diagnostics.hs
@@ -1,5 +1,6 @@
 import System(getArgs)
-import Data.Maybe(fromJust, isJust)
+import Control.Monad(when)
+import Data.Maybe(fromJust, isNothing)
 import qualified Clang.TranslationUnit as Clang
 import qualified Clang.Diagnostic as Diagnostic
 
@@ -7,9 +8,9 @@ main = do
   args <- getArgs
   index <- Clang.createIndex False False
   mtu <- Clang.parse index Nothing args [] [Clang.TranslationUnit_None]
-  let tu = if isJust mtu then fromJust mtu else error "No TXUnit!"
-      diags = Diagnostic.getDiagnostics tu
+  when (isNothing mtu) $ error "No TXUnit!"
+  let diags = Diagnostic.getDiagnostics $ fromJust mtu
       printDiag d = Diagnostic.formatDiagnostic Diagnostic.getDefaultDisplayOptions d >>=
-                  putStrLn . ("Diag:" ++)
+                    putStrLn . ("Diag:" ++)
   putStrLn $ "numDiags:" ++ show (length diags)
   mapM_ printDiag diags
