1b1576356b7896e94b3d0b2364facbdf2a09b035
Chetan T <chetant@gmail.com>
2011-04-18 07:50:04 -0400

    Fixed getFileTime

--- src/Clang/File.hs
+++ src/Clang/File.hs
@@ -2,16 +2,19 @@ module Clang.File
 (
  FFI.UnsavedFile
 ,FFI.File
-,getFileName
+,getName
+,getTime
 ,getFile
 ) where
 
 import Control.Monad((>=>))
+import Control.Applicative((<$>))
+import Data.Time.Clock.POSIX(POSIXTime, posixSecondsToUTCTime)
 import System.IO.Unsafe(unsafePerformIO)
 import qualified Clang.FFI as FFI
 
-getFileName :: FFI.File -> FilePath
-getFileName = unsafePerformIO . (FFI.getFileName >=> FFI.getCString)
-getFileTime = unsafePerformIO . FFI.getFileTime
+getName :: FFI.File -> FilePath
+getName = unsafePerformIO . (FFI.getFileName >=> FFI.getCString)
+getTime f = unsafePerformIO $ posixSecondsToUTCTime . realToFrac <$> FFI.getFileTime f
 getFile :: FFI.TranslationUnit -> FilePath -> FFI.File
 getFile t f = unsafePerformIO $ FFI.getFile t f
