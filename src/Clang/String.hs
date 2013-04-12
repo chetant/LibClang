426ccab462d3ff3211e328342b08eaa2c345b4ac
Seth Fowler <mark.seth.fowler@gmail.com>
2013-04-11 21:41:23 -0700

    Add support for hashing of CXStrings

--- src/Clang/String.hs
+++ src/Clang/String.hs
@@ -1,9 +1,11 @@
 module Clang.String
 ( FFI.CXString
 , unpack
+, hash
 ) where
 
 import Control.Monad.IO.Class
+import Data.Word
 
 import qualified Clang.Internal.FFI as FFI
 import Clang.Monad
@@ -12,3 +14,8 @@ unpack :: FFI.CXString -> ClangApp String
 unpack s = do
   str <- liftIO $ FFI.getCString s
   return $! str
+
+hash :: FFI.CXString -> ClangApp Word64
+hash s = do
+  h <- liftIO $ FFI.getCXStringHash s
+  return $! h
