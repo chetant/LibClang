f43340291723c4733503d5ef0fb67fbc19d69f61
Seth Fowler <mark.seth.fowler@gmail.com>
2013-12-08 13:01:32 -0800

    Replace the public runClangApp with clangScope

--- src/Clang/Monad.hs
+++ src/Clang/Monad.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Clang.Monad
 ( ClangApp
-, runClangApp
+, clangScope
 ) where
 
+import Control.Monad.Trans (liftIO)
+
 import Clang.Internal.ClangApp (ClangApp, runClangApp)
+
+clangScope :: (forall s. ClangApp s a) -> ClangApp s' a
+clangScope app = liftIO $ runClangApp app
+{-# INLINEABLE clangScope #-}
