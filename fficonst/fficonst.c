9438df1784a2c6bd42643baf7813b958b3480d0b
Seth Fowler <mark.seth.fowler@gmail.com>
2014-01-26 12:27:15 -0800

    Remove dependence on C11

--- fficonst/fficonst.c
+++ fficonst/fficonst.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdalign.h>
 #include <stddef.h>
 
 #include <clang-c/Index.h>
@@ -16,7 +15,7 @@ int main(int argc, char** argv)
 {
   // CXCursor constants.
   genConstant("sizeOfCXCursor", sizeof(CXCursor));
-  genConstant("alignOfCXCursor", alignof(CXCursor));
+  genConstant("alignOfCXCursor", 4);  // in C11, could use alignof()
   genConstant("offsetCXCursorKind", offsetof(CXCursor, kind));
   genConstant("offsetCXCursorXData", offsetof(CXCursor, xdata));
   genConstant("offsetCXCursorP1", offsetof(CXCursor, data));
@@ -25,20 +24,20 @@ int main(int argc, char** argv)
 
   // ParentedCursor constants.
   genConstant("sizeOfParentedCursor", sizeof(struct ParentedCursor));
-  genConstant("alignOfParentedCursor", alignof(struct ParentedCursor));
+  genConstant("alignOfParentedCursor", 4);
   genConstant("offsetParentedCursorParent", offsetof(struct ParentedCursor, parent));
   genConstant("offsetParentedCursorCursor", offsetof(struct ParentedCursor, cursor));
 
   // Inclusion constants.
   genConstant("sizeOfInclusion", sizeof(struct Inclusion));
-  genConstant("alignOfInclusion", alignof(struct Inclusion));
+  genConstant("alignOfInclusion", 4);
   genConstant("offsetInclusionInclusion", offsetof(struct Inclusion, inclusion));
   genConstant("offsetInclusionLocation", offsetof(struct Inclusion, location));
   genConstant("offsetInclusionIsDirect", offsetof(struct Inclusion, isDirect));
 
   // SourceLocation constants.
   genConstant("sizeOfCXSourceLocation", sizeof(CXSourceLocation));
-  genConstant("alignOfCXSourceLocation", alignof(CXSourceLocation));
+  genConstant("alignOfCXSourceLocation", 4);
   genConstant("offsetCXSourceLocationP1", offsetof(CXSourceLocation, ptr_data));
   genConstant("offsetCXSourceLocationP2", offsetof(CXSourceLocation, ptr_data) + sizeof(void*));
   genConstant("offsetCXSourceLocationData", offsetof(CXSourceLocation, int_data));
