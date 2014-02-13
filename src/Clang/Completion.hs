81713e87a74b110f7b973fd2dc566ccd9b3a780c
Seth Fowler <mark.seth.fowler@gmail.com>
2014-02-12 21:24:10 -0800

    Use the phantom type parameter with CodeCompleteResults. #31

--- src/Clang/Completion.hs
+++ src/Clang/Completion.hs
@@ -29,30 +29,35 @@ import qualified Clang.Internal.FFI as FFI
 import Clang.Monad
 import Clang.String (ClangString)
 
-getChunkKind :: ClangBase m => FFI.CompletionString -> Int -> ClangT s m FFI.CompletionChunkKind
+getChunkKind :: ClangBase m => FFI.CompletionString s -> Int
+             -> ClangT s m FFI.CompletionChunkKind
 getChunkKind cs i = liftIO $ FFI.getCompletionChunkKind cs i
 
-getChunkText :: ClangBase m => FFI.CompletionString -> Int -> ClangT s m (ClangString s)
+getChunkText :: ClangBase m => FFI.CompletionString s -> Int -> ClangT s m (ClangString s)
 getChunkText = FFI.getCompletionChunkText
 
-getChunkCompletionString :: ClangBase m => FFI.CompletionString -> Int ->
-                            ClangT s m FFI.CompletionString
+getChunkCompletionString :: ClangBase m => FFI.CompletionString s -> Int ->
+                            ClangT s m (FFI.CompletionString s)
 getChunkCompletionString cs i = liftIO $ FFI.getCompletionChunkCompletionString cs i
 
-getNumChunks :: ClangBase m => FFI.CompletionString -> ClangT s m Int
+getNumChunks :: ClangBase m => FFI.CompletionString s -> ClangT s m Int
 getNumChunks cs = liftIO $ FFI.getNumCompletionChunks cs
 
-getPriority :: ClangBase m => FFI.CompletionString -> ClangT s m Int
+getPriority :: ClangBase m => FFI.CompletionString s -> ClangT s m Int
 getPriority cs = liftIO $ FFI.getCompletionPriority cs
 
-getAvailability :: ClangBase m => FFI.CompletionString -> ClangT s m FFI.AvailabilityKind
+getAvailability :: ClangBase m => FFI.CompletionString s -> ClangT s m FFI.AvailabilityKind
 getAvailability cs = liftIO $ FFI.getCompletionAvailability cs
 
 defaultCodeCompleteOptions :: ClangBase m => ClangT s m [FFI.CodeCompleteFlags]
 defaultCodeCompleteOptions = do
-  defVal <- liftIO $ FFI.defaultCodeCompleteOptions
-  let val1 = if (defVal .&. 0x01) == 0x01 then return FFI.CodeComplete_IncludeMacros else mzero
-  let val2 = if (defVal .&. 0x02) == 0x02 then return FFI.CodeComplete_IncludeCodePatterns else mzero
+  defVal <- liftIO FFI.defaultCodeCompleteOptions
+  let val1 = if (defVal .&. 0x01) == 0x01
+                then return FFI.CodeComplete_IncludeMacros
+                else mzero
+  let val2 = if (defVal .&. 0x02) == 0x02
+                then return FFI.CodeComplete_IncludeCodePatterns
+                else mzero
   return $ catMaybes [val1, val2]
   
 
@@ -64,13 +69,14 @@ codeCompleteAt ::
   -> Int -- ^ Column on the line
   -> [FFI.UnsavedFile] -- ^ Unsaved files so far
   -> [FFI.CodeCompleteFlags]
-  -> ClangT s m FFI.CodeCompleteResults
-codeCompleteAt t fname l c ufs opts = liftIO $ FFI.codeCompleteAt t fname l c ufs (FFI.getCodeCompleteFlagsSum opts)
+  -> ClangT s m (FFI.CodeCompleteResults s)
+codeCompleteAt t fname l c ufs opts =
+  FFI.codeCompleteAt t fname l c ufs (FFI.getCodeCompleteFlagsSum opts)
 
-sortResults :: ClangBase m => FFI.CodeCompleteResults -> Int -> ClangT s m ()
+sortResults :: ClangBase m => FFI.CodeCompleteResults s -> Int -> ClangT s m ()
 sortResults c i = liftIO $ FFI.sortCodeCompletionResults c i
 
-getDiagnostics :: ClangBase m => FFI.CodeCompleteResults -> ClangT s m [FFI.Diagnostic s]
+getDiagnostics :: ClangBase m => FFI.CodeCompleteResults s -> ClangT s m [FFI.Diagnostic s]
 getDiagnostics c = do
   numD <- liftIO $ FFI.codeCompleteGetNumDiagnostics c
   mapM (FFI.codeCompleteGetDiagnostic c) [0..(numD-1)]
