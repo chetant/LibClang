6e49ce0b17025c82796927e2bf15f30409226062
Seth Fowler <mark.seth.fowler@gmail.com>
2014-04-20 21:08:18 -0700

    Some additional tweaks for the Remapping documentation

--- src/Clang/Remapping.hs
+++ src/Clang/Remapping.hs
@@ -13,20 +13,20 @@ import Control.Monad.IO.Class
 import qualified Clang.Internal.FFI as FFI
 import Clang.Internal.Monad
 
--- | Given a 'FilePath' for a file, return information about the remappings that
+-- | Given a 'FilePath' for a file, returns information about the remappings that
 -- apply to that file. If an error occurs, returns 'Nothing'.
 getRemappings :: ClangBase m => FilePath -> ClangT s m (Maybe (FFI.Remapping s))
 getRemappings = FFI.getRemappings
 
--- | Given a list of 'FilePath's, return information about the
+-- | Given a list of 'FilePath's, returns information about the
 -- remappings that apply to the corresponding files. If an error
 -- occurs, returns 'Nothing'.
 getRemappingsFromFileList :: ClangBase m => [FilePath] -> ClangT s m (Maybe (FFI.Remapping s))
 getRemappingsFromFileList = FFI.getRemappingsFromFileList
 
--- | Retrives a list of the remappings described by a
--- 'FFI.Remapping'. Each entry in the list consists of a pair where
--- the first element is the original filename, and the second element
+-- | Retrieves a list of the remappings described by a
+-- 'FFI.Remapping'. Each entry in the list consists of a pair.
+-- The first element is the original filename, and the second element
 -- is the filename it has been remapped to.
 getRemappedFiles :: ClangBase m => FFI.Remapping s'
                  -> ClangT s m [(FFI.ClangString s, FFI.ClangString s)]
